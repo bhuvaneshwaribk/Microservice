ccd465d917ddd9e3136e980828581ae3581fb375
bhuvaneshwari <bhuvaneshwaribk2000@gmail>
2023-11-09 11:23:16 +0530

    first commit

---
+++ src/test/java/com/bitlabs/demo/ServiceDiscovery1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitlabs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscovery1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
